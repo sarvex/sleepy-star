a36fbe53fec1b1ef427fc949ab4e47d3571cc4f6
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-07-21 22:46:25 +0530

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), image(), mdx(), partytown(), tailwind()]
+});
